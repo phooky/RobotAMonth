9f24edab39eeead7636552b38e4e8085c93e911f
Adam Mayer <me@phooky.name>
2013-01-25 14:12:30 -0500

    initial switch stop

--- January/code/eggbot/eggbot.ino
+++ January/code/eggbot/eggbot.ino
@@ -3,17 +3,21 @@
 const int gantry_enable_pin = 21;
 const int gantry_in3_pin = 20;
 const int gantry_in4_pin = 19;
+const int gantry_limit_switch = 18;
+const int gantry_station_switch = 17;
 
 const int MOTOR_OFF = 0;
 const int MOTOR_FORWARD = 1;
 const int MOTOR_BACKWARD = 2;
 const int MOTOR_BRAKE = 3;
 
-void initGantryMotor() {
+void initGantry() {
   pinMode(gantry_enable_pin, OUTPUT);
   pinMode(gantry_in3_pin, OUTPUT);
   pinMode(gantry_in4_pin, OUTPUT);
   gantryMotor(MOTOR_OFF);
+  pinMode(gantry_limit_switch, INPUT_PULLUP);
+  pinMode(gantry_station_switch, INPUT_PULLUP);
 }
 
 void gantryMotor(int direction) {
@@ -43,9 +47,23 @@ void gantryMotor(int direction) {
     break;
   }
 }
-  
+
+void gantryHome() {
+  // Clumsily PWM the motor to limit speed.
+  gantryMotor(MOTOR_BACKWARD);
+  while (digitalRead(gantry_limit_switch) == HIGH);
+  gantryMotor(MOTOR_BRAKE);
+}
+
+void gantryNextStation() {
+  gantryMotor(MOTOR_FORWARD);
+  while (digitalRead(gantry_station_switch) == LOW);
+  while (digitalRead(gantry_station_switch) == HIGH);
+  gantryMotor(MOTOR_BRAKE);
+}
+
 void setup() {
- initGantryMotor();
+ initGantry();
 }
 
 void loop() {
